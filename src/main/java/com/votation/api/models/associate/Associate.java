3976aa4fdb3f481d0221792da142f7f2cd6bb25a
Adryel Julião <adryel.juliao1@gmail.com>
2023-03-23 23:40:11 -0300

    Ajustes nos nomes e tipos dos atributos do modelo Associate

--- src/main/java/com/votation/api/models/associate/Associate.java
+++ src/main/java/com/votation/api/models/associate/Associate.java
@@ -2,29 +2,34 @@ package com.votation.api.models.associate;
 
 import com.votation.api.models.vote.Vote;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity(name = "associate")
 @Table(name = "associate")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode
+@Builder
 public class Associate {
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    @Column(name = "document_number")
-    private String cpf;
+    @Column(name = "document_number", length = 11)
+    private String documentNumber;
 
-    @OneToMany(mappedBy = "associate")
+    @OneToMany(mappedBy = "associate", fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
     private List<Vote> votes;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
 }
