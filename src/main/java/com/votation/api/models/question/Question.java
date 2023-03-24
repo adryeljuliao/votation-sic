2d6a461ea68c08feb226538d32f47fd5b05a9a43
Adryel Julião <adryel.juliao1@gmail.com>
2023-03-23 23:40:27 -0300

    Ajustes nos nomes e tipos dos atributos do modelo Question

--- src/main/java/com/votation/api/models/question/Question.java
+++ src/main/java/com/votation/api/models/question/Question.java
@@ -3,27 +3,37 @@ package com.votation.api.models.question;
 
 import com.votation.api.models.vote.Vote;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity(name = "question")
 @Table(name = "question")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode
+@Builder
 public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    @OneToMany(mappedBy = "question")
+    @Column(name = "question_description")
+    private String description;
+
+    @OneToMany(mappedBy = "question", cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
     private List<Vote> votes;
 
+    @Column(name = "final_date")
+    private LocalDateTime finalDate;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
 
 }
